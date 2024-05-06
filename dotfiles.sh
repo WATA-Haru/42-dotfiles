b13a28b9f9f4a21d019068d52676b7b04090f238
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-05-06 11:00:11 +0900

    add fish config and symbolic link

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,1 +1,2 @@
 ln -fs ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -fs ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias nvim=$HOME/.local/bin/nvim
+alias c=clear
+alias la="ls -a"
+alias lla="ls -la"
+alias francinette=$HOME/francinette/tester.sh
+alias paco=$HOME/francinette/tester.sh
+
+#- -----------------------------------------------------------------------------
+#- asdf
+#- -----------------------------------------------------------------------------
+# if status is-interactive
+#   source ~/.asdf/asdf.fish
+# end
+source ~/.asdf/asdf.fish
+
